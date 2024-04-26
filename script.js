63018030c4fc7e172276f999300de744d68926ef
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-26 06:44:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-	arr.sort();
+	
 	let cost=0;
 	while(arr.length>1)
 		{
+			arr.sort((a, b) => a - b);
 			let firstValue=arr.shift();
 			let secondValue=arr.shift();
 			let newRope=firstValue+secondValue;
 			cost+=newRope;
 			arr.push(newRope);
-			 arr.sort((a, b) => a - b);
+			
 		}
 	return cost;
   
